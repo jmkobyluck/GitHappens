634830da3945010a771f6f751a09d42cf3187e02
Emily Richter <emrichter1996@gmail.com>
2020-04-19 16:34:30 -0500

    Styling changes
    Added correctAnswer property to observableArray to allow correct answers to be shown when user chooses an answer. Fixed correctMessage and incorrectMessage placement on card.

--- Javascript-Trivia/script.js
+++ Javascript-Trivia/script.js
@@ -24,61 +24,71 @@ var quizViewModel = {
         {
             id: 1,
             question: "JavaScript was created at ____________ in the early days of the Web.",
-            answer: randomize(["Netscape", "Sun Microsystems", "Oracle", "Mozilla"])
+            answer: randomize(["Netscape", "Sun Microsystems", "Oracle", "Mozilla"]),
+            correctAnswer: "Netscape"
         },
 
         {
             id: 2,
             question: "JavaScript is typically used to specify the ____________ of web pages.",
-            answer: randomize(["behavior", "content", "presentation", "behavior, content, and presentation"])
+            answer: randomize(["behavior", "content", "presentation", "behavior, content, and presentation"]),
+            correctAnswer: "behavior"
         },
 
         {
             id: 3,
             question: "In JavaScript a variable is a symbolic name for a:",
-            answer: randomize(["value", "function", "constant", "method"])
+            answer: randomize(["value", "function", "constant", "method"]),
+            correctAnswer: "value"
         },
 
         {
             id: 4,
             question: "One of the built-in features of JavaScript is the ability to:",
-            answer: randomize(["concentrate strings", "match patterns", "control structures", "wrap objects"])
+            answer: randomize(["concentrate strings", "match patterns", "control structures", "wrap objects"]),
+            correctAnswer: "concentrate strings"
         },
 
         {
             id: 5,
             question: "All numbers in JavaScript are represented as:",
-            answer: randomize(["floating point values", "integer values", "numeric literals", "strings"])
+            answer: randomize(["floating point values", "integer values", "numeric literals", "strings"]),
+            correctAnswer: "floating point values"
         },
 
         {
             id: 6,
             question: "A return statement may appear only within the:",
-            answer: randomize(["body of a function", "return expression", "specified expression", "all of the above"])
+            answer: randomize(["body of a function", "return expression", "specified expression", "all of the above"]),
+            correctAnswer: "body of a function"
         },
 
         {
             id: 7,
             question: "To obtain the value of a property use the:",
-            answer: randomize(["dot notation or square brackets", "curly braces or square brackets", "curly braces or dot notation", "dot notation or parentheses"])
+            answer: randomize(["dot notation or square brackets", "curly braces or square brackets", "curly braces or dot notation", "dot notation or parentheses"]),
+            correctAnswer: "dot notation or square brackets"
         },
 
         {
             id: 8,
             question: "Client-side JavaScript does not provide any way to _______arbitrary files on the clients computer.",
-            answer: randomize(["write or delete", "delete or create", "write or animate", "animate or delete"])
+            answer: randomize(["write or delete", "delete or create", "write or animate", "animate or delete"]),
+            correctAnswer: "write or delete"
         },
 
         {
             id: 9,
             question: "The _____________ method was the first and only way to display computed text in a document.",
-            answer: randomize(["document.write()", "document.get()", "write.document()", "document.return()"])
+            answer: randomize(["document.write()", "document.get()", "write.document()", "document.return()"]),
+            correctAnswer: "document.write()"
         },
 
         {
             id: 10,
             question: "OnKeydown is an:",
-            answer: randomize(["event handler", "invocation", "attribute", "access key"])
+            answer: randomize(["event handler", "invocation", "attribute", "access key"]),
+            correctAnswer: "event handler"
         }
     ]),
     questionAnswered: function (ev, id, selectedAnswer) {
